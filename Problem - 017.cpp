7a701c39c7f97fa271bd6814731b6c1655135c80
Floris Velleman <florisvelleman@hotmail.com>
2014-01-10 22:57:46 +0100

    Euler: U17

--- Problem - 017.cpp
+++ Problem - 017.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <sstream>
 
 const std::vector<std::string> lowList = {
     "one","two","three","four","five","six","seven","eight","nine","ten",
@@ -10,62 +9,38 @@ const std::vector<std::string> lowList = {
 };
 
 const std::vector<std::string> medList = {
-    "twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred"
+    "twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"
 };
 
 int main() { 
-    const int upperBound = 1000;
+    int firstWords = 0;
     
-    int current = 1;
-    int solution = 11;
-
-    std::ostringstream os;
-
-    while (current != upperBound) {
-        if (current % 100 == 0) {
-            solution += lowList[current / 100 - 1].size() 
-            solution += medList[8].size();
-        }
-        else if (current % 10 == 0 && current > 19 && current < 100) {
-            solution += medList[current / 10 - 2].size();
-        }
-        else {
-            os.seekp(0);
-            os << current;
-            std::string digits = os.str();
-            int remain = 0;
-            
-            const int digitSize = digits.size();
-            
-            switch (digitSize) {
-            case 3:
-                solution += lowList[(digits[0] - '0') - 1].size() 
-                solution += medList[8].size() + 3;
-                remain = std::stoi(digits.substr(1,2));
-                break;
-            case 2:
-                if (digits[0] == '0') {
-                    remain = std::stoi(digits.substr(1,1));
-                }
-                else {
-                    remain = std::stoi(digits.substr(0,2));    
-                }
-                break;
-            default:
-                remain = current;
-                break;
-            }
-            
-            if (remain < 20) {
-                solution += lowList[remain - 1].size();
-            }
-            else if (digits[digits.size() - 1] - '0' != 0) {
-                solution += medList[remain / 10 - 2].size(); 
-                solution += lowList[(digits[digits.size() - 1] - '0') - 1].size();
-            }
-        }
-        ++current;
+    //Add the first list (0 to 19)
+    for (const auto& e : lowList) {
+        firstWords += e.size();    
     }
     
+    int secondWords = 0;
+    
+    //Add 20 to 99
+    int firstninesum = 0;
+    for (int i = 0; i < 9; i++) {
+        firstninesum += lowList[i].size();     
+    }
+    
+    secondWords += firstninesum * 8;
+    for (const auto& e : medList) {
+        secondWords += e.size() * 10;    
+    }
+    
+    //Add 100 to 999
+    const int hundred = 7 * 9;
+    const int hundredand = 9 * 990;
+    const int multiSum = (secondWords + firstWords) * 9;
+    const int prefixdigit = firstninesum * 100;
+    
+    int solution = multiSum + hundred + hundredand + prefixdigit 
+    + (secondWords + firstWords) + 11;
+
     std::cout << "Solution: " << solution << std::endl;
 }
