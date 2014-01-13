5990b8748bed256e197cd2c2a8ea3ac89761128d
Futsy <florisvelleman@hotmail.com>
2014-01-13 15:30:01 +0100

    Euler: U1
    compile time computation

--- Problem - 001 to 050/Problem - 001.cpp
+++ Problem - 001 to 050/Problem - 001.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 
+template<int n> class SumMulti {
+public:
+    static const int limitCom = ((1000 - 1)/ n);
+    static const int value = n * (limitCom * (limitCom + 1)) / 2;
+};
+ 
+template<int first, int second> class Sum {
+public:
+    static const int sumFirst = SumMulti<first>::value;
+    static const int sumSecond = SumMulti<second>::value;
+    static const int filter = SumMulti<second * first>::value;
+    
+    static constexpr int value = sumFirst + sumSecond - filter;
+};
+ 
 int main() {
-    int solution = 0;
-    
-    for(int i = 1; i <= 1000; i++) {
-        if(i % 3 == 0 || (i % 5 == 0)) {
-            solution = solution + i;
-        }
-    }
-    
-    std::cout << "Solution: " << solution;
-    return 0;
+    std::cout << Sum<3,5>::value;
 }
