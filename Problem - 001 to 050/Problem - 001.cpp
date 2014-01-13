e17d99147a8430f5d5255c22b71c2df8f81b1a7a
Floris Velleman <florisvelleman@hotmail.com>
2014-01-13 15:46:19 +0100

    Euler: U1

--- Problem - 001 to 050/Problem - 001.cpp
+++ Problem - 001 to 050/Problem - 001.cpp
@@ -1,20 +1,9 @@
 #include <iostream>
 
-template<int n> class SumMulti {
-public:
-    static const int limitCom = ((1000 - 1) / n);
-    static const int value = n * (limitCom * (limitCom + 1)) / 2;
-};
- 
-template<int first, int second> class Sum {
-public:
-    static const int sumFirst = SumMulti<first>::value;
-    static const int sumSecond = SumMulti<second>::value;
-    static const int filter = SumMulti<second * first>::value;
-    
-    static constexpr int value = sumFirst + sumSecond - filter;
-};
+constexpr unsigned int SumMulti(int n) {
+    return n * (((1000 - 1)/ n) * (((1000 - 1)/ n) + 1)) / 2;
+}
  
 int main() {
-    std::cout << Sum<3,5>::value;
+    return SumMulti(3) + SumMulti(5) - SumMulti(15);
 }
