4d5fa98867db3004a4113543b800f72f21d27a60
Futsy <florisvelleman@hotmail.com>
2014-03-26 12:28:50 +0100

    Euler: 72

---
+++ Problem - 051 to 100/Problem - 072.cpp
@@ -0,0 +1,29 @@
+#include <tuple>
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+#include "LargeInt.h"
+
+unsigned long long phi(unsigned long x) {
+    unsigned long long ret = 1;
+    unsigned long long pow;
+
+    for (unsigned long long i = 2; x != 1; i++) {
+        pow = 1;
+        while (!(x % i)) {
+            x /= i;
+            pow *= i;
+        }
+        ret *= (pow - (pow/i));
+    }
+    return ret;
+}
+
+int main() {
+    LargeInt solution = 0;
+    for (int n = 1; n <= 1000000; n++) {
+        solution += phi(n);
+    }
+    std::cout << "Solution: " << solution << std::endl;
+}
