c61c0a9b8f15f36412e981143816723713c2b10a
Floris Velleman <florisvelleman@hotmail.com>
2014-04-20 23:40:18 +0200

    Euler: U2
    Resolve at compiling.

--- Problem - 001 to 050/Problem - 002.cpp
+++ Problem - 001 to 050/Problem - 002.cpp
@@ -1,29 +1,12 @@
 #include <iostream>
 
+constexpr int EvenFibonacciSum(const int high, int old, int current) {
+    return old + current > high ? current 
+        : ((old % 2 == 0) ? old : 0) 
+        + EvenFibonacciSum(high, current, old + current);
+}
+
 int main() {
-    int solution = 0;
-    
-    const int limit = 4000000;
-    
-    int skip = 0;
-    int first = 1;
-    int second = 1;
-    
-    while ((second + first) < limit) {
-        int newSecond = second + first;
-        first = second;
-        second = newSecond;
-        
-        if (skip > 0) {
-            --skip;
-            continue;
-        }
-        else {
-            solution += second; 
-            skip = 2;
-        }
-    }
-    
-    std::cout << "Solution: " << solution;
-    return 0;
+    constexpr int solution = EvenFibonacciSum(4000000, 1, 2);
+    std::cout << "Solution: " << solution << std::endl;
 }
