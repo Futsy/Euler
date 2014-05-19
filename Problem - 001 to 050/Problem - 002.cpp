dddc4d59355f2af0e27093639e2f7c856589378c
Floris Velleman <florisvelleman@hotmail.com>
2014-05-19 19:20:09 +0200

    Euler: U2
    Compile-time solution update.

--- Problem - 001 to 050/Problem - 002.cpp
+++ Problem - 001 to 050/Problem - 002.cpp
@@ -1,12 +1,14 @@
-#include <iostream>
+/**
+ * Compile time solution
+ */
+ 
 
 constexpr int EvenFibonacciSum(const int high, int old, int current) {
     return old + current > high ? current 
-        : ((old % 2 == 0) ? old : 0) 
+        : (old % 2 == 0 ? old : 0) 
         + EvenFibonacciSum(high, current, old + current);
 }
 
 int main() {
-    constexpr int solution = EvenFibonacciSum(4000000, 1, 2);
-    std::cout << "Solution: " << solution << std::endl;
+    return EvenFibonacciSum(4000000, 1, 2);
 }
