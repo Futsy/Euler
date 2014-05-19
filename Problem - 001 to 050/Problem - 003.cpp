8e66707b76e3a7f2ffa4a93e81ef89dfb9c6835e
Floris Velleman <florisvelleman@hotmail.com>
2014-05-19 20:46:41 +0200

    Euler: U3
    Run time solution.

--- Problem - 001 to 050/Problem - 003.cpp
+++ Problem - 001 to 050/Problem - 003.cpp
@@ -1,9 +1,14 @@
+/**
+ * Run time solution
+ * 0.000067825 seconds
+ */
 #include <iostream>
 
+
 int main() {
-    int solution = 3;
+    int solution = 13;
     long long num = 600851475143;
-    
+
     while (num > 1) {
         if (num % solution == 0) {
             num = num / solution;    
@@ -12,7 +17,6 @@ int main() {
             solution += 2;    
         }
     }
-        
+    
     std::cout << "Solution: " << solution;
-    return 0;
 }
